9d1056ce2bcee997d60037e48e8182d280fea4fa
emrslmz <emrslmz57@gmail.com>
2022-01-25 12:11:12 +0300

    added

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ connectDB().then(err => {
 
 app.use(express.json({ extended: false }));
 app.use('/api/user', require('./api/userApi'));
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log('Server started'));
 
