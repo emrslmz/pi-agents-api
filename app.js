1e428e91f6ef7baa43371a7b52082ce45c576b06
emrslmz <emrslmz57@gmail.com>
2022-01-25 11:46:11 +0300

    changes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGODB_HOST, {
     useNewUrlParser: true,
