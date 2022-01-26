ff6bfce5b3f19ea1a102ec8742fb43dc27c3d196
emrslmz <emrslmz57@gmail.com>
2022-01-26 15:59:56 +0300

    added app.use header

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.use((req, res, next) => {
     res.header('Content-Type', 'application/json');
     res.header('Accept', 'application/json');
     res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 });
 
