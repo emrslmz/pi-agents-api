db71a769a79e09b3303ce0a3beea9ef9b37eb7d0
emrslmz <emrslmz57@gmail.com>
2022-01-26 17:18:57 +0300

    added app.use header

--- app.js
+++ app.js
@@ -16,17 +16,6 @@ app.get('/', (req, res) => {
     res.send('Hi, welcome to Products RESTFUL API 😍');
 });
 
-app.use((req, res, next) => {
-    res.header('Content-Type', 'application/json');
-    res.header('Accept', 'application/json');
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    next();
-});
-
 app.use('/api/agents', agentRouter);
 app.use('/api/survey-categories', surveyCategoryRouter);
 app.use('*', require('./database/index'));
